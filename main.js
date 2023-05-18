254454d46a16fa216a5f4ce67580d47a6b9ee2e7
Spoods <75pulsar75@gmail.com>
2023-05-18 10:01:13 -0400

    counter works

---
+++ main.js
@@ -0,0 +1,46 @@
+import { io } from "socket.io-client"
+
+const socket = io("ws://172.17.103.101:3000")
+var clickPerson = document.getElementById("whoClicked")
+var clickCount = document.getElementById("countClicks")
+var localCounter = 0
+var theButton = document.getElementById("theButton")
+var resetButton = document.getElementById("reset")
+
+document.querySelector("#theButton").addEventListener('click', function(){
+  sendClick()
+})
+document.querySelector("#reset").addEventListener('click', function(){
+  reset()
+})
+
+function getNewNum(arg){
+  clickPerson.innerHTML = socket.whoClicked + " clicked!"
+  localCounter = arg.substring(arg.indexOf(":") + 1, arg.indexOf(","))
+  clickCount.innerHTML = localCounter
+}
+
+function sendClick(arg){
+    console.log("clicked by you")
+    socket.emit("click")
+    getNewNum()
+}
+
+function reset(){
+  console.log("Reset!")
+  getNewNum()
+}
+
+socket.on("connectComplete", (arg) =>{
+  getNewNum(arg)
+})  
+
+socket.on("someoneClicked", (arg) =>{
+  console.log("Clicked " + arg.substring(arg.indexOf(":") + 1, arg.indexOf(",")) + " times")
+  getNewNum(arg)
+})
+
+socket.on("someoneResetClicks", (arg) =>{
+ // socket.emit("resetClicks")
+  getNewNum()
+})
