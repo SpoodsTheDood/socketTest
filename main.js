753f6c8c5ef161b35be42c9d91e361f5d743b1a5
Spoods <75pulsar75@gmail.com>
2023-05-30 17:46:46 -0400

    holy shit it works

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client"
 
+
+//HTML Elements that can be changed
 const socket = io("localhost:3000")
 var clickPerson = document.getElementById("whoClicked")
 var clickCount = document.getElementById("countClicks")
@@ -7,6 +9,8 @@ var localCounter = 0
 var theButton = document.getElementById("theButton")
 var resetButton = document.getElementById("reset")
 
+//event listeners will just call a method for simplicity
+//could likely shove methods in there at later time
 document.querySelector("#theButton").addEventListener('click', function(){
   sendClick()
 })
@@ -19,33 +23,31 @@ document.querySelector("#getNewName").addEventListener('click', function(){
 })
 
 function getNewNum(arg, stringedVers = ""){
+  //used to display counter
+  //throwing up type error but works regardless
   var splitUpVers = stringedVers.split(",")
   splitUpVers[0] = splitUpVers[0].substring((splitUpVers[0].indexOf(":") + 1))
-  console.log(splitUpVers[0])
   splitUpVers[1] = splitUpVers[1].substring(14, (splitUpVers[1].indexOf("}") - 1))
-  console.log(splitUpVers[1])
   clickPerson.innerHTML = splitUpVers[1] + " clicked!"
   localCounter = splitUpVers[0]
   clickCount.innerHTML = localCounter
 }
 
 function sendClick(arg){
-    console.log("clicked by you")
     socket.emit("click")
     getNewNum(arg)
 }
 
 function reset(arg){
   socket.emit("resetClicks")
-  console.log("Reset!")
 }
 
 function makeName(newName){
-  console.log("Submitted!")
   socket.emit("friendlyNameUpdate", newName)
 }
 
 socket.on("connectComplete", (arg) =>{
+  //log used for testing purposes
   console.log("Connected to server.")
   clickCount.innerHTML = arg.totalClicks
 })  
@@ -53,7 +55,6 @@ socket.on("connectComplete", (arg) =>{
 socket.on("someoneClicked", (arg) =>{
   console.log(arg.whoClicked + " clicked.")
   var jsonAsString = JSON.stringify(arg)
-  console.log(jsonAsString)
   getNewNum(arg, jsonAsString)
 })
 
