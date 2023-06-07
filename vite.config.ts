8c67b84cbd782368e23baf5f7553537c36c69bf7
Spoods <75pulsar75@gmail.com>
2023-06-07 10:06:56 -0400

    addded slash

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
-  base: 'https://SpoodsTheDood.github.io/socketTest',
+  base: 'https://SpoodsTheDood.github.io/socketTest/',
   build: {
     outDir: '../dist'
   },
